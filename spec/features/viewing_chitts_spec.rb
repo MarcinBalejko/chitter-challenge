177e23f66a9511e90022306dcf6794b742048409
MarcinBalejko <mbbm9@o2.pl>
2019-10-04 22:38:40 +0200

    :construction: Setting up test database

--- spec/features/viewing_chitts_spec.rb
+++ spec/features/viewing_chitts_spec.rb
@@ -1,5 +1,13 @@
+require 'pg'
+
 feature 'Viewing bookmarks' do
     scenario 'A user can see chits' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO chits VALUES(1, 'The first chit');")
+      connection.exec("INSERT INTO chits VALUES(2, 'The second chit');")
+      connection.exec("INSERT INTO chits VALUES(3, 'The third chit');")
+
       visit('/chits')
   
       expect(page).to have_content "The first chit"
