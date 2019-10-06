ccefd8bf550da3c6883bbe100c37443a3aba8d66
MarcinBalejko <mbbm9@o2.pl>
2019-10-06 22:32:55 +0200

    :white_check_mark: database helpers + database connection + chit class refactored

--- lib/chit.rb
+++ lib/chit.rb
@@ -1,35 +1,43 @@
-require 'pg'
+require_relative 'database_connection'
+require 'uri'
+require_relative 'user'
 
 class Chit
-    attr_reader :id, :text
-    def initialize(id:, text:)
-        @id = id
-        @text = text
+  attr_reader :id, :text
+  def initialize(id:, text:)
+    @id = id
+    @text = text
+  end
+
+  def self.all
+    chits = DatabaseConnection.query('SELECT * FROM chits;')
+    chits.map do |chit|
+      Chit.new(
+        id: chit['id'],
+        text: chit['text']  
+      )
     end
+  end
 
-    def self.all
-        if ENV['ENVIRONMENT'] == 'test'
-          connection = PG.connect(dbname: 'chitter_test')
-        else
-          connection = PG.connect(dbname: 'chitter')
-        end
-        result = connection.exec("SELECT * FROM chits")
-        result.map do |chit|
-          Chit.new(id: chit['id'], text: chit['text'])
-        end
-    end
-
-    def self.create(text:)
-        
-        if ENV['ENVIRONMENT'] == 'test'
-          connection = PG.connect(dbname: 'chitter_test')
-        else
-          connection = PG.connect(dbname: 'chitter')
-        end
-        result = connection.exec("INSERT INTO chits (text) VALUES('#{text}') RETURNING id, text;")
-        Chit.new(id: result[0]['id'], text: result[0]['title'])
-      end
-
-
+  def self.create(text:)
     
+    result = DatabaseConnection.query("INSERT INTO chits (text) VALUES('#{text}') RETURNING id, text;")
+    Chit.new(id: result[0]['id'], text: result[0]['text'])
+  end
+
+  def self.delete(id:)
+    DatabaseConnection.query("DELETE FROM chits WHERE id = #{id}")
+  end
+
+  def self.update(id:, text:)
+    result = DatabaseConnection.query("UPDATE chits SET text = '#{text}' WHERE id = #{id} RETURNING id, text;")
+    Chit.new(id: result[0]['id'], text: result[0]['text'])
+  end
+
+  def self.find(id:)
+    result = DatabaseConnection.query("SELECT * FROM chits WHERE id = #{id}")
+    Chit.new(id: result[0]['id'], text: result[0]['text'])
+  end
+
+  
 end
